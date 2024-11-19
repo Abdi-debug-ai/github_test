08205b06ee39d538ae5b938ffb585a9a54ca40eb
abdihalim nasir <abdihalimnasir@gmail.com>
2024-11-19 14:56:33 +0300

    update

---
+++ Dhikr.js
@@ -0,0 +1,6 @@
+const responses = {
+  // Existing responses...
+  "new question": "This is the response to the new question.",
+  "another question": "This is another custom response.",
+  "default": "I'm here to help. Can you clarify your question?"
+};
--- script.js
+++ script.js
@@ -453,3 +453,9 @@ db.collection('blogs').onSnapshot(snapshot => {
     console.log(doc.data()); // Display data or update UI
   });
 });
+const responses = {
+  // Existing responses...
+  "new question": "This is the response to the new question.",
+  "another question": "This is another custom response.",
+  "default": "I'm here to help. Can you clarify your question?"
+};
